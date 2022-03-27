1254cacd23521cc6582c8d12e4d3a3974e48cbde
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2022-03-27 15:08:09 +0300

    refactor added shared method getcookie

--- src/test/java/tests/DemoWebShopTests.java
+++ src/test/java/tests/DemoWebShopTests.java
@@ -25,10 +25,8 @@ public class DemoWebShopTests extends TestBase {
         $(".wishlist-qty").shouldHave(text("(0)"));
     }
 
-    @Test
-    @DisplayName("Successful authorization and addToWishlistTestAndCleanResults (API + UI)")
-    public void addToWishlistTestAndCleanResults() {
-        step("Get cookie by api and set it to browser", () -> {
+    public Map<String, String> getAuthorizationCookie() {
+        return step("Get cookie by api and set it to browser", () -> {
             Map<String, String> authorizationCookie =
                     given()
                             .contentType("application/x-www-form-urlencoded; charset=UTF-8")
@@ -41,60 +39,69 @@ public class DemoWebShopTests extends TestBase {
                             .extract()
                             .cookies();
 
-            String nopCustomer = "Nop.customer=" + authorizationCookie.get("Nop.customer") + ";";
-            String nopCommerce = "NOPCOMMERCE.AUTH=" + authorizationCookie.get("NOPCOMMERCE.AUTH") + ";";
-            String arrAffinity = "ARRAffinity=" + authorizationCookie.get("ARRAffinity") + ";";
+            return authorizationCookie;
+        });
+    }
 
-            step("Open minimal content, because cookie can be set when site is opened", () -> {
-                open("/Themes/DefaultClean/Content/images/logo.png");
-            });
+    @Test
+    @DisplayName("Successful authorization and addToWishlistTestAndCleanResults (API + UI)")
+    public void addToWishlistTestAndCleanResults() {
 
-            step("Set cookie to to browser", () -> {
-                getWebDriver().manage().addCookie(
-                        new Cookie("NOPCOMMERCE.AUTH", authorizationCookie.get("NOPCOMMERCE.AUTH")));
-            });
+        Map<String, String> authorizationCookie = getAuthorizationCookie();
+
+        String nopCustomer = "Nop.customer=" + authorizationCookie.get("Nop.customer") + ";";
+        String nopCommerce = "NOPCOMMERCE.AUTH=" + authorizationCookie.get("NOPCOMMERCE.AUTH") + ";";
+        String arrAffinity = "ARRAffinity=" + authorizationCookie.get("ARRAffinity") + ";";
+
+        step("Open minimal content, because cookie can be set when site is opened", () -> {
+            open("/Themes/DefaultClean/Content/images/logo.png");
+        });
+
+        step("Set cookie to to browser", () -> {
+            getWebDriver().manage().addCookie(
+                    new Cookie("NOPCOMMERCE.AUTH", authorizationCookie.get("NOPCOMMERCE.AUTH")));
+        });
 
 
-            step("Open main page", () -> {
+        step("Open main page", () -> {
+            open("");
+        });
+
+        step("Verify successful authorization", () -> {
+            $(".account").shouldHave(text(App.config.userLogin()));
+        });
+
+        int giftQuantity = 3;
+
+        step("Add some gifts on wishlist", () -> {
+            for (int i = 0; i < giftQuantity; i++) {
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .cookie(nopCustomer)
+                        .cookie(nopCommerce)
+                        .cookie(arrAffinity)
+                        .formParam("addtocart_43.EnteredQuantity", 1)
+                        .when()
+                        .post("/addproducttocart/details/43/2")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("success", is(true))
+                        .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
+                        .body("updatetopwishlistsectionhtml", is("(" + (i + 1) + ")"));
+
                 open("");
-            });
+            }
+        });
 
-            step("Verify successful authorization", () -> {
-                $(".account").shouldHave(text(App.config.userLogin()));
-            });
+        step("Check added gifts on wishlist", () -> {
+            refresh();
+            open("/wishlist");
+            $(".wishlist-qty").shouldHave(text("(" + giftQuantity + ")"));
+        });
 
-            int giftQuantity = 3;
-
-            step("Add some gifts on wishlist", () -> {
-                for (int i = 0; i < giftQuantity; i++) {
-                    given()
-                            .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                            .cookie(nopCustomer)
-                            .cookie(nopCommerce)
-                            .cookie(arrAffinity)
-                            .formParam("addtocart_43.EnteredQuantity", 1)
-                            .when()
-                            .post("/addproducttocart/details/43/2")
-                            .then()
-                            .log().all()
-                            .statusCode(200)
-                            .body("success", is(true))
-                            .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
-                            .body("updatetopwishlistsectionhtml", is("(" + (i + 1) + ")"));
-
-                    open("");
-                }
-            });
-
-            step("Check added gifts on wishlist", () -> {
-                refresh();
-                open("/wishlist");
-                $(".wishlist-qty").shouldHave(text("(" + giftQuantity + ")"));
-            });
-
-            step("clear result test after test", () -> {
-                cleanResults();
-            });
+        step("clear result test after test", () -> {
+            cleanResults();
         });
     }
 }
